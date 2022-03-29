d98a210d112a9c941c0dbf4871677ef28b5b081d
Valentin Ivanov <valentinivanov@Air-Valentin.fritz.box>
2022-03-29 11:52:36 +0300

    done

--- index.php
+++ index.php
@@ -51,14 +51,14 @@ $example_persons_array = [
 ];
 
 
-//foreach($example_persons_array as $person){
-    //$person_full_names = getPartsFromFullname($person);
-    //$person_string_name = getFullnameFromParts($person_full_names);
-    //$short_name = getShortName($person_string_name);
-    //$person_gender = getGenderFromName($person_string_name);
-    //var_dump($person_gender);
-    //die();
-//}
+foreach($example_persons_array as $person){
+    $person_full_names = getPartsFromFullname($person);
+    $person_string_name = getFullnameFromParts($person_full_names);
+    $short_name = getShortName($person_string_name);
+    $person_gender = getGenderFromName($person_string_name);
+    var_dump($person_gender);
+
+}
 
 getGenderDescription($example_persons_array);
 
@@ -86,7 +86,7 @@ function getShortName(string $person):string
     $short_name = $name[1].' '.mb_substr($name[2], 0, 1).'.';
     return $short_name;
 }
-
+// фунция определение пола
 function getGenderFromName (string $person_full_name):int{
     $gender = 0;
 
@@ -94,36 +94,36 @@ function getGenderFromName (string $person_full_name):int{
 
      foreach($name_arry as $key => $name){
         
-        $last_lether = mb_substr($name,-1);
-        $last_two_lethers = mb_substr($name,-2);
-        $last_tree_lethers = mb_substr($name,-3);
+        $last_letter = mb_substr($name,-1);
+        $last_two_letters = mb_substr($name,-2);
+        $last_tree_letters = mb_substr($name,-3);
 
         if($key == 2){
-            if($last_two_lethers == 'ич'){
+            if($last_two_letters == 'ич'){
                 $gender++;
             }
 
-            if($last_tree_lethers == 'внa'){
+            if($last_tree_letters == 'внa'){
                 $gender--;
             }
         }
         
         if($key == 1){
-            if($last_lether == "й" || $last_lether == "н"){
+            if($last_letter == "й" || $last_letter == "н"){
                 $gender++;
             }
-            if($last_lether == "а"){
+            if($last_letter == "а"){
                 $gender--;
             }
         }
         
         if($key == 0){
 
-            if($last_lether == 'в'){
+            if($last_letter == 'в'){
                 $gender++;
             }
 
-            if($last_two_lethers == 'вa'){
+            if($last_two_letters == 'вa'){
                 $gender--;
             }
         
@@ -133,17 +133,17 @@ function getGenderFromName (string $person_full_name):int{
      
 
     if ($gender > 0) {
-        $gender = 1; // 
+        $gender = 1; // мужского пола
     }elseif ($gender < 0) {
-        $gender = -1; // 
+        $gender = -1; // женского пола
     }else{
-        $gender = 0; //
+        $gender = 0; // неопределенный пол
     }
 
     return $gender;
 }
 
-
+// функция определение определения полового состава аудитории
 function getGenderDescription (array $example_persons_array) {
     $male = 0;
     $female = 0;
@@ -175,11 +175,37 @@ function getGenderDescription (array $example_persons_array) {
 
     echo 'Гендерный состав аудитории:';
     echo  '<br>';
-    echo 'Мужчины - '.$male_percent.' %';
+    echo 'Мужчины - '.$male_percent.' %'; // процент мужской пол
     echo  '<br>';
-    echo 'Женщины - '.$female_percent.' %';
+    echo 'Женщины - '.$female_percent.' %';// процент женского пол
     echo  '<br>';
-    echo 'Не удалось определить -'.$unknow_percent.' %';
+    echo 'Не удалось определить -'.$unknow_percent.' %'; // процент неопределенный пол
 }
+
+//функция для определения «идеальной» пары
+function getPerfectPartner(array $data, int $repeat = 1) {
+    $data = array_values($data); 
+    $result = []; // Контейнер с результатом
+
+    foreach ($example_persons_array as $outerKey => $outerElement) {
+        $internalData = array_slice($data, $outerKey + 1); 
+        foreach ($internalData as $innerKey => $innerElement) {
+            $pair = [$outerElement, $innerElement]; 
+            $result[] = $pair;
+
+            if ($repeat > 1) {
+                $reversedPair = [$innerElement, $outerElement]; 
+
+                for ($i = 2; $i <= $repeat; $i++) {
+                    $result[] = ($i % 2 == 0) ? $reversedPair : $pair;
+                }
+            }
+        }
+    }
+
+    return $result;
+}
+
+
 ?>
 </html>
